3e96792f534482dd908d9e19b3ec630152e89cd4
shreyjain9 <122493682+shreyjain9@users.noreply.github.com>
2025-04-24 19:30:25 -0400

    Create scripts2.js

---
+++ scripts2.js
@@ -0,0 +1,10 @@
+/*
+    Student Name: Shrey Jain
+    File Name: scripts2.js
+    Date: 04/24/2025
+*/
+
+//jQuery for hero image to consume the header window space
+    $(document).ready(function(){
+    $('.hero').height($(window).height());
+});
